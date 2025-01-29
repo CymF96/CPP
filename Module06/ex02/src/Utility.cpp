9117270d17f4be1d44f4525a70e8373baa238f83
cofische <cofische@student.42london.com>
2025-01-29 11:57:27 +0000

    adding correction of ex02 ref //ptr

--- Module06/ex02/src/Utility.cpp
+++ Module06/ex02/src/Utility.cpp
@@ -39,15 +39,26 @@ void identify(Base *p) {
 	}
 };
 
-void identify(Base &p) {
-    try {
-        if (dynamic_cast<A *>(&p))
-            std::cout << "type of p is: " BOLD RED "A" RESET << std::endl;
-        else if (dynamic_cast<B *>(&p))
-            std::cout << "type of p is: " BOLD GREEN "B" RESET << std::endl;
-        else if (dynamic_cast<C *>(&p))
-            std::cout << "type of p is: " BOLD BLUE "C" RESET << std::endl;
-    } catch(const std::exception& e) {
-        std::cout << "*P cannot be identified: " << e.what() << std::endl;
-    }
-};
+void identify(Base& p) {
+	try {
+		A& a = dynamic_cast<A&>(p);
+		(void)a;
+		std::cout << "type of p is: " BOLD RED "A" RESET << std::endl;
+	} catch (std::exception& e) {
+		;
+	}
+	try {
+		B& b = dynamic_cast<B&>(p);
+		(void)b;
+		std::cout << "type of p is: " BOLD RED "B" RESET << std::endl;
+	} catch (std::exception& e) {
+		;
+	}
+	try {
+		C& c = dynamic_cast<C&>(p);
+		(void)c;
+		std::cout << "type of p is: " BOLD RED "C" RESET << std::endl;
+	} catch (std::exception& e) {
+		;
+	}
+}
