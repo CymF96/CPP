15463a0b0f89ad5340ad3321c14be033b7a6bbae
CymF96 <cym.fischer@gmail.com>
2025-02-03 11:13:09 +0000

    correction Module07

--- Module07/ex00/main.cpp
+++ Module07/ex00/main.cpp
@@ -6,56 +6,56 @@
 /*   By: cofische <cofische@student.42london.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 16:12:16 by cofische          #+#    #+#             */
-/*   Updated: 2025/01/28 16:41:49 by cofische         ###   ########.fr       */
+/*   Updated: 2025/02/03 10:47:32 by cofische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Whatever.hpp"
 
-int main( void ) {
-	int a = 2;
-	int b = 3;
-	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-	std::string c = "chaine1";
-	std::string d = "chaine2";
-	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
-	return 0;
-}
-
-// class Awesome
-// {
-//   public:
-//     Awesome(void) : _n(0) {}
-//     Awesome( int n ) : _n( n ) {}
-//     Awesome & operator= (Awesome & a) { _n = a._n; return *this; }
-//     bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
-//     bool operator!=( Awesome const & rhs ) const{ return (this->_n != rhs._n); }
-//     bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
-//     bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
-//     bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
-//     bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
-//     int get_n() const { return _n; }
-//   private:
-//     int _n;
-// };
-// std::ostream & operator<<(std::ostream & o, const Awesome &a) { o << a.get_n(); return o; }
-
-// int main(void)
-// {
-// 	Awesome a(2), b(4);
-
-//         swap(a, b);
-//         std::cout << a << " " << b << std::endl;
-//         std::cout << max(a, b) << std::endl;
-//         std::cout << min(a, b) << std::endl;
-//         return (0);
+// int main( void ) {
+// 	int a = 2;
+// 	int b = 3;
+// 	::swap( a, b );
+// 	std::cout << "a = " << a << ", b = " << b << std::endl;
+// 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+// 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+// 	std::string c = "chaine1";
+// 	std::string d = "chaine2";
+// 	::swap(c, d);
+// 	std::cout << "c = " << c << ", d = " << d << std::endl;
+// 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+// 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+// 	return 0;
 // }
+
+class Awesome
+{
+  public:
+    Awesome(void) : _n(0) {}
+    Awesome( int n ) : _n( n ) {}
+    Awesome & operator= (Awesome & a) { _n = a._n; return *this; }
+    bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
+    bool operator!=( Awesome const & rhs ) const{ return (this->_n != rhs._n); }
+    bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
+    bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
+    bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
+    bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
+    int get_n() const { return _n; }
+  private:
+    int _n;
+};
+std::ostream & operator<<(std::ostream & o, const Awesome &a) { o << a.get_n(); return o; }
+
+int main(void)
+{
+	Awesome a(2), b(4);
+
+        swap(a, b);
+        std::cout << a << " " << b << std::endl;
+        std::cout << max(a, b) << std::endl;
+        std::cout << min(a, b) << std::endl;
+        return (0);
+}
 
 // int main() {
 
--- Module07/ex01/main.cpp
+++ Module07/ex01/main.cpp
@@ -6,74 +6,74 @@
 /*   By: cofische <cofische@student.42london.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 17:02:23 by cofische          #+#    #+#             */
-/*   Updated: 2025/01/28 17:54:34 by cofische         ###   ########.fr       */
+/*   Updated: 2025/02/03 10:48:23 by cofische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
-// class Awesome
-// {
-//   public:
-//     Awesome( void ) : _n( 42 ) { return; }
-//     int get( void ) const { return this->_n; }
-//   private:
-//     int _n;
-// };
+class Awesome
+{
+  public:
+    Awesome( void ) : _n( 42 ) { return; }
+    int get( void ) const { return this->_n; }
+  private:
+    int _n;
+};
 
-// std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
-// {
-//   o << rhs.get();
-//   return o;
-// }
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+{
+  o << rhs.get();
+  return o;
+}
 
-// template< typename T >
-// void print( T& x )
-// {
-//   std::cout << x << std::endl;
-//   return;
-// }
-
-// int main() {
-//   int tab[] = { 0, 1, 2, 3, 4 };
-//   Awesome tab2[5];
-
-//   iter( tab, 5, print<const int> );
-//   iter( tab2, 5, print<Awesome> );
-
-//   return 0;
-// }
+template< typename T >
+void print( T& x )
+{
+  std::cout << x << std::endl;
+  return;
+}
 
 int main() {
-	int array[] = {1, 2, 3};
-	size_t lenghtArray = sizeof(array)/sizeof(array[0]);
-	
-	std::cout << BOLD BLUE "Original Int Array" RESET << std::endl; 
-	iter(array, lenghtArray, &printIter);
-	iter(array, lenghtArray, &multiply);
-	std::cout << BOLD GREEN "New Int Array after iter: " RESET << std::endl;
-	iter(array, lenghtArray, &printIter);
-	std::cout << std::endl;
-	
-	std::string strArray[] = {"hello", "how", "are" , "you?"};
-	size_t lenStr = sizeof(strArray) / sizeof(strArray[0]);
-	
-	std::cout << BOLD BLUE "Original string Array" RESET << std::endl; 
-	iter(strArray, lenStr, &printIter);
-	iter(strArray, lenStr, &upperchar);
-	std::cout << BOLD GREEN "New string after iter: " RESET << std::endl;
-	iter(strArray, lenStr, &printIter);
-	std::cout << std::endl;
-	
-	double arrayD[] = {146.256, -258.765489, 3.1415926535};
-	size_t lenArrayD = sizeof(arrayD)/sizeof(arrayD[0]);
-	
-	std::cout << BOLD BLUE "Original Double Array" RESET << std::endl; 
-	iter(arrayD, lenArrayD, &printIter);
-	iter(arrayD, lenArrayD, &formating);
-	std::cout << BOLD GREEN "New Double Array after iter: " RESET << std::endl;
-	iter(arrayD, lenArrayD, &printIter);
-	std::cout << std::endl;
-	
-	return 0;
+  int tab[] = { 0, 1, 2, 3, 4 };
+  Awesome tab2[5];
+
+  iter( tab, 5, print<const int> );
+  iter( tab2, 5, print<Awesome> );
+
+  return 0;
 }
+
+// int main() {
+// 	int array[] = {1, 2, 3};
+// 	size_t lenghtArray = sizeof(array)/sizeof(array[0]);
+	
+// 	std::cout << BOLD BLUE "Original Int Array" RESET << std::endl; 
+// 	iter(array, lenghtArray, &printIter);
+// 	iter(array, lenghtArray, &multiply);
+// 	std::cout << BOLD GREEN "New Int Array after iter: " RESET << std::endl;
+// 	iter(array, lenghtArray, &printIter);
+// 	std::cout << std::endl;
+	
+// 	std::string strArray[] = {"hello", "how", "are" , "you?"};
+// 	size_t lenStr = sizeof(strArray) / sizeof(strArray[0]);
+	
+// 	std::cout << BOLD BLUE "Original string Array" RESET << std::endl; 
+// 	iter(strArray, lenStr, &printIter);
+// 	iter(strArray, lenStr, &upperchar);
+// 	std::cout << BOLD GREEN "New string after iter: " RESET << std::endl;
+// 	iter(strArray, lenStr, &printIter);
+// 	std::cout << std::endl;
+	
+// 	double arrayD[] = {146.256, -258.765489, 3.1415926535};
+// 	size_t lenArrayD = sizeof(arrayD)/sizeof(arrayD[0]);
+	
+// 	std::cout << BOLD BLUE "Original Double Array" RESET << std::endl; 
+// 	iter(arrayD, lenArrayD, &printIter);
+// 	iter(arrayD, lenArrayD, &formating);
+// 	std::cout << BOLD GREEN "New Double Array after iter: " RESET << std::endl;
+// 	iter(arrayD, lenArrayD, &printIter);
+// 	std::cout << std::endl;
+	
+// 	return 0;
+// }
