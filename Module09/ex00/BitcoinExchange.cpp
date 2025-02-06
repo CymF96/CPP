90a853f0679d9560832e432adeb70ae8eb4ca464
cofische <cofische@student.42london.com>
2025-02-06 17:37:09 +0000

    ex00 ready for correction

--- Module09/ex00/BitcoinExchange.cpp
+++ Module09/ex00/BitcoinExchange.cpp
@@ -23,12 +23,12 @@ BTC::BTC(const std::string &filename) : mapKey(0), mapValue(0), database("CSV/da
 		dataMapping(database, 1);
 		database.close();
 	} else
-		throw BTC::BTCERROR("Faild generating database\n");
+		throw BTC::BTCERROR("Failed generating database\n");
 	if (conversionFile.is_open()) {
 		dataMapping(conversionFile, 2);
 		conversionFile.close();
 	} else
-		throw BTC::BTCERROR("Faild reading conversionFile\n");
+		throw BTC::BTCERROR("Failed reading conversionFile\n");
 }
 BTC::~BTC() {}
 BTC::BTC(const BTC &other) : tempBTC(other.tempBTC), mapKey(other.mapKey), mapValue(other.mapValue), btc(other.btc) {}
@@ -46,7 +46,7 @@ BTC &BTC::operator=(const BTC &other) {
 /**********GETTERS**************/
 /*******************************/
 
-const std::map<std::string, float> &BTC::getMap() {
+const map &BTC::getMap() {
 	return btc;
 }
 
@@ -64,31 +64,43 @@ keyType BTC::getKey() const {
 
 void BTC::dataMapping(std::ifstream &base, int flag) {
 	std::string line;
-	std::getline(base, line); 
-	if (!isdigit(line[0]))
-		std::getline(base, line);
+	std::string fstLine;
+	if (!std::getline(base, fstLine)) {
+		throw BTC::BTCERROR("Error: Empty database file\n");
+		return;
+	}
+	if (isdigit(fstLine[0]))
+		line = fstLine;
+	else 
+		(std::getline(base, line));
 	switch (flag) {
-		case 1: 
-			while (std::getline(base, line)) {	
+		case 1:
+			do {
 				if (checkFormat(line))
 					Mapping(line);
 				else {
-					throw BTC::BTCERROR ("database creation failed\n");
-					return ;
+					throw BTC::BTCERROR("database creation failed\n");
+					return;
 				}
-			}
+			} while (std::getline(base, line));
+			break;
+
 		case 2:
-			while (std::getline(base, line)) {
+			do {
 				try {
 					checkFormatFile(line);
 					std::string temp = line.substr(0, 10);
 					findValue(temp);
 					printConversion(temp);
-
 				} catch (std::exception &e) {
 					std::cout << "Error: " << e.what();
 				}
-			}
+			} while (std::getline(base, line));
+			break;
+
+		default:
+			throw BTC::BTCERROR("Invalid flag provided\n");
+			return;
 	}
 };
 
@@ -117,6 +129,7 @@ bool BTC::checkFormat(std::string &line) {
 		throw BTC::BTCERROR("wrong date format (YYYY-MM-DD)\n");
 		return false;
 	}
+	//CREATE A SEPARATE FUNCTION FOR CHECKING DATE VALIDITY??
 	if (month == 2 && day == 29) {
 		if ((!(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)))) {
 			throw BTC::BTCERROR("date isn't correct\n");
@@ -134,56 +147,21 @@ bool BTC::checkFormat(std::string &line) {
 	return true;		
 };
 
-void BTC::findValue(const std::string &key) {
-	mapKey = btc.find(key);
-	if (mapKey == btc.end()) {
-		// std::cout << "key: " << key << ", mapKey: not found" << std::endl;
-		findClosest(key);
-	}	
-	else {
-		mapValue = mapKey->second;
-		// std::cout << "key: " << key << ", mapKey: " << mapKey->first << std::endl;
-	}
-};
-
-keyType BTC::findClosest(const std::string &key) {
-	keyType closestHigh = btc.begin();
-	keyType closestLow = btc.end();
-	while (closestHigh != btc.end() && closestHigh->first < key)
-		++closestHigh;
-	if (closestHigh == btc.end())
-		--closestHigh;
-	while (closestLow != btc.begin() && closestLow->first > key)
-		++closestLow;
-	if (closestHigh == btc.end() || 
-		(closestHigh->first < key && closestLow->first >= key)) {
-		return closestHigh;
-	} else {
-		return closestLow;
-	}
-};
-
 void BTC::checkFormatFile(std::string &line) {
 	int year, month, day;
 	char dash1, dash2, spe;
 	int daysInMonth[] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	
-	// std::cout << line << std::endl;
 
 	std::istringstream iss(line);
 	iss >> year >> dash1 >> month >> dash2 >> day >> spe >> value;
-
-	// std::cout << "year: " << year << std::endl;
-	// std::cout << "dash1: " << dash1 << std::endl;
-	// std::cout << "month: " << month << std::endl;
-	// std::cout << "dash2: " << dash2 << std::endl;
-	// std::cout << "day: " << day << std::endl;
-	// std::cout << "spe: " << spe << std::endl;
-	// std::cout << "value: " << value << std::endl;
 	
 	if (iss.fail() || dash1 != '-' || dash2 != '-' || spe != '|' || !isdigit(line[0]) || line[10] != ' ' || line[12] != ' ') {
 		throw BTC::BTCERROR("invalid input. Format = (YYYY-MM-DD | price)\n");
 	}
+	else if (value < 0 || value > 1000)  {
+		throw BTC::BTCERROR("value requested isn't positive or too high (0 to 1000 only)\n");
+	}
+	//SEPARATE FUNCTION FOR CHECKING VALIDITY OF DATE ?? STRUCT FOR DATE + VALUE ??
 	else if (month == 2 && day == 29) {
 		if ((!(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)))) {
 			throw BTC::BTCERROR("date isn't correct\n");
@@ -192,9 +170,33 @@ void BTC::checkFormatFile(std::string &line) {
 	else if ((year < 1900 || year > 2025) && (month < 1 || month > 12) && (day <= daysInMonth[month])) {
 		throw BTC::BTCERROR("date isn't correct\n");
 	}
-	else if (value < 0 || value > 1000)  {
-		throw BTC::BTCERROR("value requested isn't positive or too high (0 to 1000 only)\n");
-	}		
+	
+};
+
+void BTC::findValue(const std::string &key) {
+	mapKey = btc.find(key);
+	if (mapKey == btc.end()) {
+		mapKey = findClosest(key);
+		mapValue = mapKey->second;
+	}	
+	else
+		mapValue = mapKey->second;
+};
+
+keyType BTC::findClosest(const std::string &key) {
+	keyType dateAfter = btc.begin();
+	keyType dateBefore = btc.end();
+	--dateBefore;
+	while (dateAfter != btc.end() && dateAfter->first < key)
+		++dateAfter;
+	if (dateAfter == btc.end())
+		--dateAfter;
+	while (dateBefore != btc.begin() && dateBefore->first > key)
+		--dateBefore;
+	if ((dateBefore->first < key)  && dateBefore->first >= key)
+		return dateBefore;
+	else
+		return dateAfter;
 };
 
 /*******************************/
@@ -208,12 +210,12 @@ const char* BTC::BTCERROR::what() const throw() { return message; };
 /********OTHER FUNCTIONS********/
 /*******************************/
 
-std::ostream &operator<<(std::ostream &os, BTC &data) {
-	std::map<std::string, float>::const_iterator it;
-	for (it = data.getMap().begin(); it != data.getMap().end(); ++it)
-		os << "[" << it->first << "] = " << it->second << std::endl;
-	for (it = data.tempBTC.begin(); it != data.tempBTC.end(); ++it)
-		os << "[" << it->first << "] = " << it->second << std::endl;
-	return os;
-};
+// std::ostream &operator<<(std::ostream &os, BTC &data) {
+// 	std::map<std::string, float>::const_iterator it;
+// 	for (it = data.getMap().begin(); it != data.getMap().end(); ++it)
+// 		os << "[" << it->first << "] = " << it->second << std::endl;
+// 	for (it = data.tempBTC.begin(); it != data.tempBTC.end(); ++it)
+// 		os << "[" << it->first << "] = " << it->second << std::endl;
+// 	return os;
+// };
 
