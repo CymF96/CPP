bbafdf27a0ca0de819376330b4d4acc1db266ea3
cofische <cofische@student.42london.com>
2025-02-11 19:05:14 +0000

    adding PmergeMe ex

--- Module09/ex02/PmergeMe.cpp
+++ Module09/ex02/PmergeMe.cpp
@@ -3,37 +3,128 @@
 /*                                                        :::      ::::::::   */
 /*   PmergeMe.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cofische <cofische@student.42london.com    +#+  +:+       +#+        */
+/*   By: cofische <cofische@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 17:40:56 by cofische          #+#    #+#             */
-/*   Updated: 2025/02/10 17:44:51 by cofische         ###   ########.fr       */
+/*   Updated: 2025/02/11 17:58:00 by cofische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/******************************************************************************/
-/*
-Array: [10, 15, 25, 48, 65, 120, 121, 170, 189, 200]
-Insert 133:
+#include "PmergeMe.hpp"
 
-Iteration 1:
-  - Mid = 4 (arr[mid] = 65)
-  - 133 > 65, so move right: left = 5
+ddeq sequence;
+ddeq pairs;
+ddeq oddNumb;
+vec vec1;
+int flag = 0;
 
-Iteration 2:
-  - Mid = 7 (arr[mid] = 170)
-  - 133 < 170, so move left: right = 6
+/*******************************/
+/********CONSTRUCTORS***********/
+/*******************************/
 
-Iteration 3:
-  - Mid = 5 (arr[mid] = 120)
-  - 133 > 120, so move right: left = 6
+Pmerge::Pmerge() {};
+Pmerge::~Pmerge() {};
+Pmerge::Pmerge(const Pmerge &other) { (void)other; };
+Pmerge &Pmerge::operator=(const Pmerge &other) { (void)other; return *this; };
 
-Iteration 4:
-  - Mid = 6 (arr[mid] = 121)
-  - 133 > 121, so move right: left = 7
+/*******************************/
+/********MEMBER FUNCTIONS*******/
+/*******************************/
 
-Now left = 7, the correct position to insert 133.
+void Pmerge::sorted(char **input, size_t size) {
+	generateSequence(input, size);
+	pairing(sequence);
+	std::cout << "size: " << pairs.size() << "\n";
+	int pos = 0;
+	for (size_t i = 0; i < pairs.size(); i++) {
+		itv start = pairs[i].begin();
+		itv end = pairs[i].end();
+		std::cout << pos++ << ": ";
+		for (; start != end; ++start) {
+			std::cout << *start << " ";
+		}
+		std::cout << std::endl;
+	}
+	std::cout << "\n\n";
+	std::cout << "size: " << oddNumb.size() << "\n";
+	pos = 0;
+	for (size_t i = 0; i < oddNumb.size(); i++) {
+		itv start = oddNumb[i].begin();
+		itv end = oddNumb[i].end();
+		std::cout << pos++ << ": ";
+		for (; start != end; ++start) {
+			std::cout << *start << " ";
+		}
+		std::cout << std::endl;
+	}
+};
 
-Final array: [10, 15, 25, 48, 65, 120, 121, 133, 170, 189, 200]
-*/
-//USE OF LIST + VECTOR
-/******************************************************************************/
+void Pmerge::generateSequence(char **input, size_t size) {
+	int nb1;
+	size_t i = 1;
+	while (i < size) {
+		try {
+			nb1 = convertInt(input[i]);
+		} catch (std::exception &e) {
+			std::cout << BOLD RED "Error: " RESET << e.what();
+			flag = 1;
+			return ;
+		}
+		vec1.push_back(nb1);
+		sequence.push_back(vec1);
+		vec1.clear();
+		i++;
+	}
+};
+
+void Pmerge::pairing(ddeq &inputSequence) {
+	size_t size = inputSequence.size();
+	if (size <= 2) {
+		pairs = inputSequence;
+		return;
+	}
+	ddeq tempPairs;
+	size_t ind = 0;
+	// int pos = 0;
+	while (ind + 1 < size) {
+		// std::cout << pos++ << ": " << sequence[ind] << "\n";
+		int num1 = inputSequence[ind].back();
+		int num2 = inputSequence[ind + 1].back();
+		if (num2 < num1)
+			inputSequence[ind].swap(inputSequence[ind + 1]);
+		vec newPair = inputSequence[ind];
+		newPair.insert(newPair.end(), inputSequence[ind + 1].begin(), inputSequence[ind + 1].end());
+		tempPairs.push_back(newPair);
+		newPair.clear();
+		ind += 2;
+	}
+	if (ind < size)
+		oddNumb.push_front(inputSequence.back());
+	pairing(tempPairs);
+}
+
+/*******************************/
+/********EXCEPTIONS*************/
+/*******************************/
+
+Pmerge::PmergeError::PmergeError(const char *msg) : message(msg) {};
+const char *Pmerge::PmergeError::what() const throw() { return message; };
+
+/*******************************/
+/********HELPER FUNCTIONS*******/
+/*******************************/
+int convertInt(char *nbr) {
+	std::istringstream iss(static_cast<std::string>(nbr));
+	// std::cout << nbr << std::endl;
+	long nb;
+	iss >> nb;
+
+	if (iss.fail())
+		throw Pmerge::PmergeError("unable to convert char* to int\n");
+	else if (nb > std::numeric_limits<int>::max()) 
+		throw Pmerge::PmergeError("number too large (INT_MAX limit reach)\n");
+	else if (nb < 0)
+		throw Pmerge::PmergeError("negative number not accepted\n");
+	return static_cast<int>(nb);
+};
+
--- Module09/ex02/PmergeMe.hpp
+++ Module09/ex02/PmergeMe.hpp
@@ -3,10 +3,52 @@
 /*                                                        :::      ::::::::   */
 /*   PmergeMe.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cofische <cofische@student.42london.com    +#+  +:+       +#+        */
+/*   By: cofische <cofische@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 17:34:52 by cofische          #+#    #+#             */
-/*   Updated: 2025/02/10 18:01:52 by cofische         ###   ########.fr       */
+/*   Updated: 2025/02/11 16:39:25 by cofische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <exception>
+#include <sstream>
+#include <limits>
+#include <deque>
+#include <algorithm>
+#include "../Colors.hpp"
+
+typedef std::vector<int> vec;
+typedef std::deque<int> deq;
+typedef vec::iterator itv;
+typedef deq::iterator itd;
+typedef std::deque<vec> ddeq;
+
+class Pmerge {
+	public:
+		static void sorted(char ** input, size_t size);
+		static void generateSequence(char ** input, size_t size);
+		static void pairing(ddeq &inputSequence);
+		// static void printList();
+		// static itl binarySearch(int key);
+		// static void binaryInsert(int key);
+
+		class PmergeError : public std::exception {
+			private:
+				const char* message;
+			public:
+				PmergeError(const char *msg);
+				virtual const char* what() const throw();
+		};
+		
+	private:
+		Pmerge();
+		~Pmerge();
+		Pmerge(const Pmerge &other);
+		Pmerge &operator=(const Pmerge &other);
+};
+
+int convertInt(char *nbr);
+void convertToList();
