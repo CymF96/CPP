65709d078ad8b68898847947d746a19dde010725
CymF96 <cym.fischer@gmail.com>
2025-02-12 18:41:43 +0000

    exercise 02 M09

--- Module09/ex02/PmergeMe.cpp
+++ Module09/ex02/PmergeMe.cpp
@@ -3,128 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   PmergeMe.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cofische <cofische@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cofische <cofische@student.42london.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 17:40:56 by cofische          #+#    #+#             */
-/*   Updated: 2025/02/11 17:58:00 by cofische         ###   ########.fr       */
+/*   Updated: 2025/02/12 17:37:57 by cofische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
-ddeq sequence;
-ddeq pairs;
-ddeq oddNumb;
-vec vec1;
-int flag = 0;
-
-/*******************************/
-/********CONSTRUCTORS***********/
-/*******************************/
-
-Pmerge::Pmerge() {};
-Pmerge::~Pmerge() {};
-Pmerge::Pmerge(const Pmerge &other) { (void)other; };
-Pmerge &Pmerge::operator=(const Pmerge &other) { (void)other; return *this; };
 
 /*******************************/
 /********MEMBER FUNCTIONS*******/
 /*******************************/
 
-void Pmerge::sorted(char **input, size_t size) {
-	generateSequence(input, size);
-	pairing(sequence);
-	std::cout << "size: " << pairs.size() << "\n";
-	int pos = 0;
-	for (size_t i = 0; i < pairs.size(); i++) {
-		itv start = pairs[i].begin();
-		itv end = pairs[i].end();
-		std::cout << pos++ << ": ";
-		for (; start != end; ++start) {
-			std::cout << *start << " ";
-		}
-		std::cout << std::endl;
-	}
-	std::cout << "\n\n";
-	std::cout << "size: " << oddNumb.size() << "\n";
-	pos = 0;
-	for (size_t i = 0; i < oddNumb.size(); i++) {
-		itv start = oddNumb[i].begin();
-		itv end = oddNumb[i].end();
-		std::cout << pos++ << ": ";
-		for (; start != end; ++start) {
-			std::cout << *start << " ";
-		}
-		std::cout << std::endl;
-	}
-};
-
-void Pmerge::generateSequence(char **input, size_t size) {
-	int nb1;
-	size_t i = 1;
-	while (i < size) {
-		try {
-			nb1 = convertInt(input[i]);
-		} catch (std::exception &e) {
-			std::cout << BOLD RED "Error: " RESET << e.what();
-			flag = 1;
-			return ;
-		}
-		vec1.push_back(nb1);
-		sequence.push_back(vec1);
-		vec1.clear();
-		i++;
-	}
-};
-
-void Pmerge::pairing(ddeq &inputSequence) {
-	size_t size = inputSequence.size();
-	if (size <= 2) {
-		pairs = inputSequence;
-		return;
-	}
-	ddeq tempPairs;
-	size_t ind = 0;
-	// int pos = 0;
-	while (ind + 1 < size) {
-		// std::cout << pos++ << ": " << sequence[ind] << "\n";
-		int num1 = inputSequence[ind].back();
-		int num2 = inputSequence[ind + 1].back();
-		if (num2 < num1)
-			inputSequence[ind].swap(inputSequence[ind + 1]);
-		vec newPair = inputSequence[ind];
-		newPair.insert(newPair.end(), inputSequence[ind + 1].begin(), inputSequence[ind + 1].end());
-		tempPairs.push_back(newPair);
-		newPair.clear();
-		ind += 2;
-	}
-	if (ind < size)
-		oddNumb.push_front(inputSequence.back());
-	pairing(tempPairs);
-}
-
-/*******************************/
-/********EXCEPTIONS*************/
-/*******************************/
-
-Pmerge::PmergeError::PmergeError(const char *msg) : message(msg) {};
-const char *Pmerge::PmergeError::what() const throw() { return message; };
-
 /*******************************/
 /********HELPER FUNCTIONS*******/
 /*******************************/
+
+int jacobsthal(int jIndex) {
+	return round((pow(2, jIndex + 1) + pow(-1, jIndex)) / 3); 
+}
+
+size_t getSize(char** arr) {
+    size_t count = 0;
+    while (arr[count]) {
+        count++;
+    }
+    return count;
+}
+
 int convertInt(char *nbr) {
 	std::istringstream iss(static_cast<std::string>(nbr));
-	// std::cout << nbr << std::endl;
+
 	long nb;
 	iss >> nb;
 
 	if (iss.fail())
-		throw Pmerge::PmergeError("unable to convert char* to int\n");
+		throw PmergeError("unable to convert char* to int\n");
 	else if (nb > std::numeric_limits<int>::max()) 
-		throw Pmerge::PmergeError("number too large (INT_MAX limit reach)\n");
+		throw PmergeError("number too large (INT_MAX limit reach)\n");
 	else if (nb < 0)
-		throw Pmerge::PmergeError("negative number not accepted\n");
+		throw PmergeError("negative number not accepted\n");
 	return static_cast<int>(nb);
 };
+
+
 
--- Module09/ex02/PmergeMe.hpp
+++ Module09/ex02/PmergeMe.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   PmergeMe.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cofische <cofische@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cofische <cofische@student.42london.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/07 17:34:52 by cofische          #+#    #+#             */
-/*   Updated: 2025/02/11 16:39:25 by cofische         ###   ########.fr       */
+/*   Created: 2025/02/12 17:50:22 by cofische          #+#    #+#             */
+/*   Updated: 2025/02/12 18:39:56 by cofische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,38 +17,149 @@
 #include <sstream>
 #include <limits>
 #include <deque>
+#include <math.h>
 #include <algorithm>
 #include "../Colors.hpp"
 
-typedef std::vector<int> vec;
-typedef std::deque<int> deq;
-typedef vec::iterator itv;
-typedef deq::iterator itd;
-typedef std::deque<vec> ddeq;
-
-class Pmerge {
+template <typename T>
+class PmergeMe {
 	public:
-		static void sorted(char ** input, size_t size);
-		static void generateSequence(char ** input, size_t size);
-		static void pairing(ddeq &inputSequence);
-		// static void printList();
-		// static itl binarySearch(int key);
-		// static void binaryInsert(int key);
+		PmergeMe(T &container) { FJAS(container, 1); };
+		PmergeMe(const PmergeMe &other) {(void)other; };
+		PmergeMe& operator=(const PmergeMe &other) { (void)other; return *this; };
+		~PmergeMe() {};
 
-		class PmergeError : public std::exception {
-			private:
-				const char* message;
-			public:
-				PmergeError(const char *msg);
-				virtual const char* what() const throw();
-		};
+		typedef typename T::iterator Iterator;
+		typedef typename std::vector<Iterator>::iterator it_itv;
+		typedef typename std::vector<Iterator> itv;
 		
+		bool is_odd;
+		int pairs;
+		itv main;
+		itv pend;
+
+
+		void pairing (T &container, int level) {
+			pairs = container.size() / level;
+			if (pairs < 2)
+				return;
+			Iterator start = container.begin();
+			Iterator last = next(container.begin(), level * (pairs));
+			Iterator end = next(last, -(is_odd * level));
+			int jump = 2 * level;
+			for (Iterator it = start; it != end; std::advance(it, jump))
+			{
+				Iterator this_pair = next(it, level - 1);
+				Iterator next_pair = next(it, level * 2 - 1);
+				if (*this_pair > *next_pair)
+				{
+					_swap_pair(this_pair, level);
+				}
+			}
+			FJAS(container, level * 2);
+			containerInitialisation(container, level, pairs);
+		}
+		
+		void containerInitialisation(T &container, int level, int pairs) {
+			main.insert(main.end(), next(container.begin(), level - 1));
+			main.insert(main.end(), next(container.begin(), level * 2 - 1));
+
+
+			for (int i = 4; i <= pairs; i += 2)
+			{
+				pend.insert(pend.end(), next(container.begin(), level * (i - 1) - 1));
+				main.insert(main.end(), next(container.begin(), level * i - 1));
+			}
+		}
+		
+		void jacobsthalSorting(T &container) {
+			int prev_jacob = jacobsthal(1);
+			int insertion = 0;
+			for (int k = 2;; k++)
+			{
+				int curr_jacob = jacobsthal(k);
+				int jacobsthal_diff = curr_jacob - prev_jacob;
+				int offset = 0;
+				if (jacobsthal_diff > static_cast<int>(pend.size()))
+					break;
+				int nbr_of_times = jacobsthal_diff;
+				it_itv pend_it = next(pend.begin(), jacobsthal_diff - 1);
+				it_itv bound_it = next(main.begin(), curr_jacob + insertion);
+				while (nbr_of_times)
+				{
+					it_itv idx = std::upper_bound(main.begin(), bound_it, *pend_it, comp);
+					it_itv inserted = main.insert(idx, *pend_it);
+					nbr_of_times--;
+					pend_it = pend.erase(pend_it);
+					std::advance(pend_it, -1);
+					offset += (inserted - main.begin()) == curr_jacob + insertion;
+					bound_it = next(main.begin(), curr_jacob + insertion - offset);
+				}
+				prev_jacob = curr_jacob;
+				insertion += jacobsthal_diff;
+				offset = 0;
+			}
+		}
+		
+		void binarySorting(T &container, int level, int pairs) {
+			Iterator last = next(container.begin(), level * (pairs));
+			Iterator end = next(last, -(is_odd * level));
+			for (size_t i = 0; i < pend.size(); i++) {
+				it_itv curr_pend = next(pend.begin(), i);
+				it_itv curr_bound = next(main.begin(), main.size() - pend.size() + i);
+				it_itv idx = std::upper_bound(main.begin(), curr_bound, *curr_pend, comp);
+				main.insert(idx, *curr_pend);
+			}
+			if (is_odd) {
+				Iterator odd_pair = next(end, level - 1);
+				it_itv idx = std::upper_bound(main.begin(), main.end(), odd_pair, comp);
+				main.insert(idx, odd_pair);
+			}
+		}
+		
+		void storingSortedSequence(T &container, int level) {
+			std::vector<int> copy;
+			copy.reserve(container.size());
+			for (it_itv it = main.begin(); it != main.end(); it++) {
+				for (int i = 0; i < level; i++) {
+					Iterator pair_start = *it;
+					std::advance(pair_start, -level + i + 1);
+					copy.insert(copy.end(), *pair_start);
+				}
+			}
+		}
+
+		void finalSequence(T &container) {
+			Iterator container_it = container.begin();
+			std::vector<int>::iterator copy_it = copy.begin();
+			while (copy_it != copy.end())
+			{
+				*container_it = *copy_it;
+				container_it++;
+				copy_it++;
+			}
+		}
+		
+		void FJAS(T& container, int level) {
+			pairing(container, level);
+			jacobsthalSorting(container);
+			binarySorting(container, level);
+			storingSortedSequence(container, level);
+			finalSequence(container);
+		}
+
+		bool comp(T lv, T rv) { return *lv < *rv; };
+};
+
+class PmergeError : public std::exception {
 	private:
-		Pmerge();
-		~Pmerge();
-		Pmerge(const Pmerge &other);
-		Pmerge &operator=(const Pmerge &other);
+		const char *message;
+
+	public:
+		PmergeError(const char *msg) : message(msg) {};
+		virtual const char *what() const throw() { return message; };
 };
 
 int convertInt(char *nbr);
-void convertToList();
+int jacobsthal(int jIndex);
+size_t getSize(char** arr);
