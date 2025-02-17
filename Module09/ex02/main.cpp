81bc14aa778ac0498b06792f5c2cb55796af9038
CymF96 <cym.fischer@gmail.com>
2025-02-17 16:11:08 +0000

    correction and cleaning of ex02

--- Module09/ex02/main.cpp
+++ Module09/ex02/main.cpp
@@ -13,12 +13,16 @@
 #include "PmergeMe.hpp"
 
 int main(int ac, char **av) {
+	//	Format error management
 	if (ac < 2) {
 		std::cout << BOLD RED "Format error: " RESET "./PmergeMe 1 2 3" << std::endl;
 		return -1;
 	}
+
+	//	1st Container sorting -- Vector
 	size_t size = getSize(av);
 	std::vector<int> vec;
+
 	try {
 		generateSequenceVector(vec, av, size);
 		std::cout << BOLD BLUE "\nBefore : " RESET;
@@ -26,14 +30,20 @@ int main(int ac, char **av) {
 	} catch (std::exception &e) {
 		std::cerr << BOLD RED "Error: " RESET << e.what();
 	}
+
 	clock_t start = clock();
 	PmergeMe<std::vector<int> > vecSort(vec);
 	clock_t end = clock();
+
 	std::cout << BOLD GREEN "After: " RESET << vecSort;
 	double duration = static_cast<double>(end - start) / CLOCKS_PER_SEC;
-	std::cout << "\t --> Time to process a range of " BOLD MAGENTA << size - 1 << RESET " elements with " BOLD " std::vector : " MAGENTA << duration << RESET " sec" << std::endl; 
+	std::cout << "\t --> Time to process a range of " BOLD MAGENTA << size - 1 << RESET " elements with " BOLD " std::vector : " MAGENTA << std::fixed << std::setprecision(6) << duration << RESET " sec" << std::endl; 
 	std::cout << std::endl;
+
+
+	//	2nd Container sorting -- Deque
 	std::deque<int> deq;
+
 	try {
 		generateSequenceDeque(deq, av, ac);
 		std::cout << BOLD BLUE "Before : " RESET;
@@ -42,12 +52,15 @@ int main(int ac, char **av) {
 		std::cerr << "Error: " << e.what() << std::endl;
 		return 1;
 	}
+
 	start = clock();
 	PmergeMe<std::deque<int> > deqSort(deq);
 	end = clock();
+
 	std::cout << BOLD GREEN "After: " RESET << deqSort;
 	duration = static_cast<double>(end - start) / CLOCKS_PER_SEC;
-	std::cout << "\t --> Time to process a range of " BOLD MAGENTA << size - 1 << RESET " elements with " BOLD " std::deque : " MAGENTA << duration << RESET " sec" << std::endl; 
+	std::cout << "\t --> Time to process a range of " BOLD MAGENTA << size - 1 << RESET " elements with " BOLD " std::deque : " MAGENTA << std::fixed << std::setprecision(6) << duration << RESET " sec" << std::endl; 
 	std::cout << std::endl;
+
 	return 0;
 }
